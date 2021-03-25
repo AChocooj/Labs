b2b60f91fdfc11335a71673f3c629154789cd9dc
Allison Chocooj <agu18834@uvg.edu.gt>
2021-03-24 19:36:10 -0600

    poco a poco iré colocando solo lo que necesito

--- LAB5/LAB5/LAB5.ino
+++ LAB5/LAB5/LAB5.ino
@@ -1,6 +1,26 @@
+/*Allison Chocooj 
+18834
+MOSI - pin 11
+MISO - pin 12 
+CLK - pin 13 
+CS - pin 4
+*/
+
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+File root;
+int Byte = 0;
+
 void setup() {
   // put your setup code here, to run once:
-//hola
+Serial.begin(9600);
+while(!Serial){
+  ;//Esperamos a que se conecte el puerto
+}
+
+
 }
 
 void loop() {
