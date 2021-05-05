f82140625d4ee48da5fc51d6ccac2ce784ab5ceb
Allison Chocooj <agu18834@uvg.edu.gt>
2021-05-05 10:01:34 -0600

    el laboratorio 6 lo copie de mi workspace y lo pegue en mi repositorio

---
+++ LAB6/Lab6/Debug/main.d
@@ -0,0 +1,81 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+main.obj: D:/TivaWare/driverlib/gpio.c
+main.obj: D:/TivaWare/inc/hw_gpio.h
+main.obj: D:/TivaWare/inc/hw_ints.h
+main.obj: D:/TivaWare/inc/hw_memmap.h
+main.obj: D:/TivaWare/inc/hw_sysctl.h
+main.obj: D:/TivaWare/inc/hw_types.h
+main.obj: D:/TivaWare/driverlib/debug.h
+main.obj: D:/TivaWare/driverlib/gpio.h
+main.obj: D:/TivaWare/driverlib/interrupt.h
+main.obj: D:/TivaWare/driverlib/sysctl.c
+main.obj: D:/TivaWare/inc/hw_nvic.h
+main.obj: D:/TivaWare/inc/hw_flash.h
+main.obj: D:/TivaWare/driverlib/cpu.h
+main.obj: D:/TivaWare/driverlib/sysctl.h
+
+../main.c:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+D:/TivaWare/driverlib/gpio.c:
+
+D:/TivaWare/inc/hw_gpio.h:
+
+D:/TivaWare/inc/hw_ints.h:
+
+D:/TivaWare/inc/hw_memmap.h:
+
+D:/TivaWare/inc/hw_sysctl.h:
+
+D:/TivaWare/inc/hw_types.h:
+
+D:/TivaWare/driverlib/debug.h:
+
+D:/TivaWare/driverlib/gpio.h:
+
+D:/TivaWare/driverlib/interrupt.h:
+
+D:/TivaWare/driverlib/sysctl.c:
+
+D:/TivaWare/inc/hw_nvic.h:
+
+D:/TivaWare/inc/hw_flash.h:
+
+D:/TivaWare/driverlib/cpu.h:
+
+D:/TivaWare/driverlib/sysctl.h:
+
