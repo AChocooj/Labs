dd6add2ece60e0dace3706fa501d432c7bbf00f9
Allison Chocooj <agu18834@uvg.edu.gt>
2021-05-12 15:56:30 -0600

    lab7 que saque del workspace del code composer

---
+++ LAB7/lab7/Debug/main.d
@@ -0,0 +1,120 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+main.obj: D:/TivaWare/inc/tm4c123gh6pm.h
+main.obj: D:/TivaWare/inc/hw_memmap.h
+main.obj: D:/TivaWare/inc/hw_types.h
+main.obj: D:/TivaWare/inc/hw_ints.h
+main.obj: D:/TivaWare/driverlib/sysctl.h
+main.obj: D:/TivaWare/driverlib/sysctl.c
+main.obj: D:/TivaWare/inc/hw_nvic.h
+main.obj: D:/TivaWare/inc/hw_sysctl.h
+main.obj: D:/TivaWare/inc/hw_flash.h
+main.obj: D:/TivaWare/driverlib/cpu.h
+main.obj: D:/TivaWare/driverlib/debug.h
+main.obj: D:/TivaWare/driverlib/interrupt.h
+main.obj: D:/TivaWare/driverlib/interrupt.c
+main.obj: D:/TivaWare/driverlib/gpio.h
+main.obj: D:/TivaWare/driverlib/gpio.c
+main.obj: D:/TivaWare/inc/hw_gpio.h
+main.obj: D:/TivaWare/driverlib/timer.h
+main.obj: D:/TivaWare/driverlib/timer.c
+main.obj: D:/TivaWare/inc/hw_timer.h
+main.obj: D:/TivaWare/driverlib/uart.h
+main.obj: D:/TivaWare/driverlib/uart.c
+main.obj: D:/TivaWare/inc/hw_uart.h
+main.obj: D:/TivaWare/driverlib/pin_map.h
+main.obj: D:/TivaWare/driverlib/rom.h
+main.obj: D:/TivaWare/grlib/grlib.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/string.h
+main.obj: D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/xlocale/_string.h
+
+../main.c:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+D:/TivaWare/inc/tm4c123gh6pm.h:
+
+D:/TivaWare/inc/hw_memmap.h:
+
+D:/TivaWare/inc/hw_types.h:
+
+D:/TivaWare/inc/hw_ints.h:
+
+D:/TivaWare/driverlib/sysctl.h:
+
+D:/TivaWare/driverlib/sysctl.c:
+
+D:/TivaWare/inc/hw_nvic.h:
+
+D:/TivaWare/inc/hw_sysctl.h:
+
+D:/TivaWare/inc/hw_flash.h:
+
+D:/TivaWare/driverlib/cpu.h:
+
+D:/TivaWare/driverlib/debug.h:
+
+D:/TivaWare/driverlib/interrupt.h:
+
+D:/TivaWare/driverlib/interrupt.c:
+
+D:/TivaWare/driverlib/gpio.h:
+
+D:/TivaWare/driverlib/gpio.c:
+
+D:/TivaWare/inc/hw_gpio.h:
+
+D:/TivaWare/driverlib/timer.h:
+
+D:/TivaWare/driverlib/timer.c:
+
+D:/TivaWare/inc/hw_timer.h:
+
+D:/TivaWare/driverlib/uart.h:
+
+D:/TivaWare/driverlib/uart.c:
+
+D:/TivaWare/inc/hw_uart.h:
+
+D:/TivaWare/driverlib/pin_map.h:
+
+D:/TivaWare/driverlib/rom.h:
+
+D:/TivaWare/grlib/grlib.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/string.h:
+
+D:/CodeComposer/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/xlocale/_string.h:
+
